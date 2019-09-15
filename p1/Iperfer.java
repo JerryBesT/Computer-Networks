7acbd9f407fcd14d819fbc1e7a197e1ccab4228f
Zhenyu Zou <zzou@royal-02.cs.wisc.edu>
2019-09-15 13:13:27 -0500

    part1

--- p1/Iperfer.java
+++ p1/Iperfer.java
@@ -32,7 +32,7 @@ public class Iperfer {
 			int time = Integer.parseInt(args[6]);
 
 			int port = Integer.parseInt(args[4]);
-			if(port < 1024 || port > 65536)
+			if(port < 1024 || port > 65535)
 				err("Error: port number must be in the range 1024 to 65536");
 			
 			client(host, port, time);
@@ -44,7 +44,7 @@ public class Iperfer {
 				err("Error: invalid arguments");
 
 			int port = Integer.parseInt(args[2]);
-			if(port < 1024 || port > 65536)
+			if(port < 1024 || port > 65535)
 				err("Error: port number must be in the range 1024 to 65536");
 
 			server(port);
@@ -57,16 +57,17 @@ public class Iperfer {
 		BufferedReader in = new BufferedReader (new InputStreamReader (clientSoc.getInputStream()));
 		long end = System.currentTimeMillis() + time * 1000;
 
-		int b_sent = 0;
+		long b_sent = 0;
 		while(System.currentTimeMillis() < end) {
 			byte[] data = new byte[1000];	
-			out.write(data);
+			out.write(data, 0, 1000);
 			b_sent += 1000;
 		}
 
-		int mb = b_sent / 1000;
-		int mbps = mb * 8 / time;
-		System.out.println("sent=" + mb + " KB rate=" + mbps + " Mbps");
+		long mb = b_sent / 1000;
+		// double may not hold long time period
+		double mbps = mb * 8 / time;
+		System.out.println("sent=" + mb + " KB rate=" + String.format("%.3f", mbps / 1000.0) + " Mbps");
 		return;
 	}
 
@@ -80,25 +81,26 @@ public class Iperfer {
 		DataInputStream in = new DataInputStream(clientSoc.getInputStream());
 		long start = System.currentTimeMillis();
 
-		int b_received = 0;
-		while(in.readInt() == 1000) {
-			byte[] data = new byte[1000];
-			in.readFully(data, 0, 1000);
-			boolean check = false;
+		long b_received = 0;
+		byte[] data = new byte[1000];
+		while(in.read(data) > 0) {
+			boolean check = true;
 			for(byte i : data)
-				if(i != 0)
+				if(i != 0) {
 					System.out.println("data corrupted");
-					
+					check = false;
+				}
+
 			if(check) {
 				b_received += 1000;
-				System.out.println("Server side received a chunk");
 			}
 		}
 
-		int mb = b_received / 1000;
-		long time = (System.currentTimeMillis() - start) / 1000;
-		long mbps = mb * 8 / time;
-		System.out.println("received=" + mb + " KB rate=" + mbps + " Mbps");
+		long mb = b_received / 1000;
+		// double may not hold long time period
+		double time = (System.currentTimeMillis() - start) / 1000.0;
+		double mbps = mb * 8 / time;
+		System.out.println("received=" + mb + " KB rate=" + String.format("%.3f", mbps / 1000.0) + " Mbps");
 		return;
 	}
 }
